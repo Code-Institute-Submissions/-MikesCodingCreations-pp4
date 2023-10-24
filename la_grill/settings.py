43f68a7ab135352a06c1cb413579ef73e02b3851
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-10-24 19:59:34 +0000

    changes made to setting, to finish building menu list view

--- la_grill/settings.py
+++ la_grill/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-pp4-app.herokuapp.com', 'localhost', '8000-mikescoding-restaurants-epwinn2uuue.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['restaurant-pp4-app.herokuapp.com', '8000-mikescoding-restaurants-epwinn2uuue.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'bookings',
     'menu',
 ]
@@ -129,12 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
