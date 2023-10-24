2e6718c1ed8b88c1f486599ff2c4aa413bf7c40e
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-10-24 17:59:56 +0000

    slug field added

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Menu(models.Model):
@@ -7,6 +8,7 @@ class Menu(models.Model):
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='menu/')
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self): 
         return self.name
