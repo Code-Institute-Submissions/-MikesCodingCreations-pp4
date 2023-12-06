4a4bdb6b96cb59b6f6284ddc0c93754a13146c5a
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-12-06 21:29:38 +0000

    Changes made to styling of site, links also

--- la_grill/urls.py
+++ la_grill/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls', namespace='menu')),
-    # path('blog/', include('blog.urls', namespace='blog')),
     path('about-us/', include('aboutus.urls', namespace='aboutus')),
     path('reserve_table/', include('reservation.urls', namespace='reservation')),
     path('', include('home.urls', namespace='home')),
