77df70498f3610468d754aa0fa71d75986a9661e
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-12-07 15:14:46 +0000

    Form is now working, saving to admin

--- reservation/views.py
+++ reservation/views.py
@@ -1,8 +1,25 @@
+# from .models import Reservation
+# from django.shortcuts import render
+# from .forms import ReserveTableForm
+
+# from reservation.models import Reservation
+
+# def reserve_table(request):
+#     reserve_form = ReserveTableForm()
+
+#     if request.method == 'POST':
+#         reserve_form = ReserveTableForm(request.POST)
+
+#     if reserve_form.is_valid():
+#         reserve_form.save()
+
+#     context = {'form': reserve_form}
+
+#     return render(request, 'Reservation/reservation.html', context)
+
 from .models import Reservation
 from django.shortcuts import render
 from .forms import ReserveTableForm
-
-from reservation.models import Reservation
 
 def reserve_table(request):
     reserve_form = ReserveTableForm()
@@ -10,9 +27,9 @@ def reserve_table(request):
     if request.method == 'POST':
         reserve_form = ReserveTableForm(request.POST)
 
-    if reserve_form.is_valid():
-        reserve_form.save()
+        if reserve_form.is_valid():
+            reserve_form.save()
+            # Redirect or perform any action after successful form submission
 
     context = {'form': reserve_form}
-
     return render(request, 'Reservation/reservation.html', context)
