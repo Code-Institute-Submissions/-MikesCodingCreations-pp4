94297dbc5dc203a4df81e84b54faf4d873df35b4
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-02-05 16:45:06 +0000

    debug turned off

--- la_grill/settings.py
+++ la_grill/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mikescodingcreat-pp4-qh3gxsjrf49.ws-eu106.gitpod.io', 'la-grill-pp4-40d10c869885.herokuapp.com']
 
