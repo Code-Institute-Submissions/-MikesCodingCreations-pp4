3edbfdbb3bfa57e5f45a06f7a7806c277519cfda
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-02-11 19:07:53 +0000

    Implementation of comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
-from .models import Category, Post
+from .models import Category, Post, Comment
 from django.contrib import admin
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Comment)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Comment
 from django.http import HttpResponse
 from taggit.models import Tag
+from .forms import CommentForm
 # Create your views here.
 
-
+# POST LIST
 def post_list(request):
     post_list = Post.objects.all()
 
@@ -14,20 +15,35 @@ def post_list(request):
 
     return render(request, 'Post/post_list.html', context)
 
+# POST DETAIL
 def post_detail(request, id):
     post_detail = Post.objects.get(id=id)
     categories = Category.objects.all()
     all_tags = Tag.objects.all()
+    comments = Comment.objects.filter(post=post_detail)
+    comment_form = CommentForm()
+    
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post_detail
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
 
     context = {
         'post_detail': post_detail,
         'categories': categories,
         'all_tags': all_tags,
+        'comments': comments,
+        'comment_form': comment_form,
     }
 
     return render(request, 'Post/post_detail.html', context)
 
-
+# TAGS
 def post_by_tag(request, tag):
     post_by_tag = Post.objects.filter(tags__name__in=[tag])
     context = {
@@ -36,6 +52,7 @@ def post_by_tag(request, tag):
 
     return render(request, 'Post/post_list.html', context)
 
+# CATEGORIES
 def post_by_category(request, category):
     post_by_category = Post.objects.filter(category__category_name=category)
     context = {
