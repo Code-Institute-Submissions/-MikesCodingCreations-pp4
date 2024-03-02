4399ab55844ec5db17524abc71d8789a9c3739db
Mikes Coding Creations <mikescodingcreations@gmail.com>
2024-03-02 17:51:22 +0000

    Changes made to code styling, removed comments

--- la_grill/settings.py
+++ la_grill/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WSECRET_KEY = os.environ['SECRET_KEY']ction secret!
 # SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+b0lm(3%_9hr7'
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'p@ei#69*b*zz3u4yig-$()@cy^l(+x9&@6ypx+b0lm(3%_9hr7')
 
 
 
